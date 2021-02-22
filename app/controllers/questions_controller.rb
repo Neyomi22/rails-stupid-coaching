d56fa32caad0484cc2e3dd30435b95f763cb157b
Amanda Paranamana <neyomi22@hotmail.com>
2021-02-22 18:00:30 +1100

    updated a condition statement

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   def answer
     if params["question"] == "I am going to work"
       @answer = "Great!"
-    elsif params.include? "?"
+    elsif params["question"].include? "?"
       @answer = "Silly question, get dressed and go to work!."
     else 
       @answer = "I don't care, get dressed and go to work!"
