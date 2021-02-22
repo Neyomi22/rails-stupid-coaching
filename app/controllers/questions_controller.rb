a12f51bb98c1b7d912a70d7e9dcc71c264c8da3f
Amanda Paranamana <neyomi22@hotmail.com>
2021-02-22 17:59:45 +1100

    updated a p tag to answer html file

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   def answer
     if params["question"] == "I am going to work"
       @answer = "Great!"
-    elsif params["question"].include? "?"
+    elsif params.include? "?"
       @answer = "Silly question, get dressed and go to work!."
     else 
       @answer = "I don't care, get dressed and go to work!"
